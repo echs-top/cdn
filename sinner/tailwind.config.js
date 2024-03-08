3d015aee73aacbc54c559d741af38ad0764a601a
ECHSFXY <88482848+echs-top@users.noreply.github.com>
2024-03-08 17:41:22 +0800

    Update tailwind.config.js

--- sinner/tailwind.config.js
+++ sinner/tailwind.config.js
@@ -1,1 +1,23 @@
-module.exports = { darkMode: 'class', content: ["./*.{html,js,php}","./lib/*.{html,js,php}","./assets/js/*.{html,js,php}"], theme: { }, plugins: [], safelist: [ 'block', 'sm:block', 'md:block', 'xl:block', '2xl:block', 'hidden', 'sm:hidden', 'md:hidden', 'xl:hidden', '2xl:hidden', { pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/, variants: ['sm','md', 'lg', 'xl', '2xl'], }, ], }
+module.exports = {
+  darkMode: 'class',
+  content: ["./*.{html,js,php}","./lib/*.{html,js,php}","./assets/js/*.{html,js,php}"],
+  theme: {
+    },
+  plugins: [],
+  safelist: [
+    'block',
+    'sm:block',
+    'md:block',
+    'xl:block',
+    '2xl:block',
+    'hidden',
+    'sm:hidden',
+    'md:hidden',
+    'xl:hidden',
+    '2xl:hidden',
+    {
+      pattern: /grid-cols-(1|2|3|4|5|6|7|8|9|10|11|12)/,
+      variants: ['sm','md', 'lg', 'xl', '2xl'],
+    },
+  ],
+}
