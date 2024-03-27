b046d6f2ca2fa691386a38711ca573ee9775d277
ECHSFXY <88482848+echs-top@users.noreply.github.com>
2024-03-28 01:45:42 +0800

    Delete sinner directory

--- sinner/assets/code/lines.js
+++
@@ -1,9 +0,0 @@
-function lines(){
-var linescon=document.querySelectorAll("pre code");
-if(linescon){
-linescon.forEach((value, index) => {if(linescon[index].querySelectorAll("ul li").length<=0){
-var c=linescon[index].innerHTML;
-linescon[index].innerHTML="<ul><li>" + c.replace(/\n/g,"\n</li><li>") +"\n</li></ul>";}
-});
-}
-  }
