75ddf303550e6394fed749c47e7ebfcdd0ad7b0a
ECHSFXY <88482848+echs-top@users.noreply.github.com>
2024-06-13 07:28:51 +0800

    Delete rssant directory

--- rssant/analytics.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    window.plausible = window.plausible || function() {
-        (window.plausible.q = window.plausible.q || []).push(arguments) }
-    var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript'; g.async = true; g.defer = true;
-    g.setAttribute('data-domain', 'rss.anyant.com');
-    g.src = 'https://p.anyant.com/' + 'js/plausible.js'; s.parentNode.insertBefore(g, s);
-})();
