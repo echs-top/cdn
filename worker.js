cecef0a65fe1e1b442275e7f34e16986fc8654ba
ECHSFXY <88482848+echs-top@users.noreply.github.com>
2026-06-07 22:07:07 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,18 +1,5 @@
 export default {
   async fetch(request, env, ctx) {
-    // 跨域
-    const corsHeaders = {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "*",
-      "Access-Control-Allow-Headers": "*",
-      "Content-Security-Policy": "frame-ancestors *",
-    };
-
-    // 预检
-    if (request.method === "OPTIONS") {
-      return new Response(null, { headers: corsHeaders });
-    }
-
     const url = new URL(request.url);
 
     try {
@@ -26,11 +13,6 @@ export default {
 
       // 重写Response
       const newResponse = new Response(response.body, response);
-      
-      // 设置跨域头
-      Object.keys(corsHeaders).forEach(key => {
-        newResponse.headers.set(key, corsHeaders[key]);
-      });
 
       return newResponse;
 
